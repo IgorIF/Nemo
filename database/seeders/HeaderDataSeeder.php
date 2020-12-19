33f757dbb69b368370c65057ec296232f5273b9f
IgorIF <igarif1992@gmail.com>
2020-12-20 01:15:22 +0200

    add header data migration and seeder

---
+++ database/seeders/HeaderDataSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class HeaderDataSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('trainers')->insert([
+            'name' => 'text_1',
+            'text' => 'Плавать раньше, чем ходить!'
+        ]);
+
+        DB::table('trainers')->insert([
+            'name' => 'text_2',
+            'text' => 'Индивидуальные занятия плаванием для детей от 1,5 месяцев до 7 лет в Люберцах, Чертаново, Марьино и Академическом районах'
+        ]);
+    }
+}
