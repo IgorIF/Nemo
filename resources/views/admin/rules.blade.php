4ac499448819278c2b9808bdb475932a58e17c48
IgorIF <igarif1992@gmail.com>
2021-01-23 18:51:13 +0200

    shows rules

--- resources/views/admin/rules.blade.php
+++ resources/views/admin/rules.blade.php
@@ -3,21 +3,27 @@
 <div class="all-rules">
     <div class="rule1">
         <ul>
-            <li></li>
+            @foreach($ruleCategories[0]->ruleItems as $rule)
+                <li>{!! $rule->text !!}</li>
+            @endforeach
         </ul>
     </div>
 
     <p id="text_{{ $texts->get(64)->id }}" contenteditable="true">{!! $texts->get(64)->text !!}</p>
     <div class="rule2">
         <ul>
-            <li></li>
+            @foreach($ruleCategories[1]->ruleItems as $rule)
+                <li>{!! $rule->text !!}</li>
+            @endforeach
         </ul>
     </div>
 
     <p id="text_{{ $texts->get(65)->id }}" contenteditable="true">{!! $texts->get(65)->text !!}</p>
     <div class="rule2">
         <ul>
-            <li></li>
+            @foreach($ruleCategories[2]->ruleItems as $rule)
+                <li>{!! $rule->text !!}</li>
+            @endforeach
         </ul>
     </div>
 </div>
