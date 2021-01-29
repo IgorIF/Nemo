d22326c64bf2d1ca8071608d6ad6dbb1a4fd0dd4
IgorIF <igarif1992@gmail.com>
2021-01-30 00:53:13 +0200

    rule item add

--- public/js/admin.js
+++ public/js/admin.js
@@ -265,12 +265,14 @@ $(document).ready(function () {
     }
 
     function saveNewRuleItem() {
-        // TODO
         let data = new FormData($modalAddItem.find('form')[0]);
         data.append('ruleCategoryId', ruleCategoryId);
         let url = 'admin/rules';
         ajax('POST', url, data, function (response) {
-            console.log(response);
+            let ruleItem = response.ruleItem;
+            ruleItemAdd(ruleItem);
+            $modalAddItem.modal('hide');
+            clearFields($modalAddItem)
         }, function (error) {
             if (error.status === 422) {
                 let errors = error.responseJSON.errors;
@@ -455,7 +457,6 @@ $(document).ready(function () {
     function securityItemAdd(securityItem) {
         let container = $('div[id="securityCategory_' + securityCategoryId + '"]').children('ul');
 
-
         let content = '<div id="securityItem_' + securityItem.id + '" >' +
                             '<li>' +
                                 '<span id="securityItem_text" contenteditable="true">' + securityItem.text + '</span>' +
@@ -465,6 +466,22 @@ $(document).ready(function () {
                                 '</svg>' +
                             '</li>' +
                         '</div>';
+
+        container.append(content);
+    }
+
+    function ruleItemAdd(ruleItem) {
+        let container = $('div[id="ruleCategory_' + ruleCategoryId + '"]').children('ul');
+
+        let content = '<div id="ruleItem_' + ruleItem.id + '">' +
+                            '<li>' +
+                                '<span id="ruleItem_text" contenteditable="true">' + ruleItem.text + '</span>' +
+                                '<svg style="float: right" id="ruleItem_delete_btn" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">' +
+                                    '<path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"></path>' +
+                                    '<path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"></path>' +
+                                '</svg>' +
+                            '</li>' +
+                        '</div>'
 
         container.append(content);
     }
