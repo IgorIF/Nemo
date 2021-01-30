81d9310417b4bc502bf5e716dd6870cd14628f38
IgorIF <igarif1992@gmail.com>
2021-01-30 19:04:12 +0200

    some fix bug

--- public/js/admin.js
+++ public/js/admin.js
@@ -166,7 +166,8 @@ $(document).ready(function () {
         let id = $(element).parents('[id^="ruleItem"]').attr('id').split('_')[1];
         let url = 'admin/rules/' + id;
 
-        ajax('PUT', url, data);
+        if (text !== initialText)
+            ajax('PUT', url, data);
     }
 
     function saveMedicalCertificateText(element) {
@@ -175,7 +176,8 @@ $(document).ready(function () {
         let id = $(element).parents('[id^="medicalCertificate"]').attr('id').split('_')[1];
         let url = 'admin/medicalCertificates/' + id;
 
-        ajax('PUT', url, data);
+        if (text !== initialText)
+            ajax('PUT', url, data);
     }
 
     function deleteTrainer(element) {
@@ -497,7 +499,7 @@ $(document).ready(function () {
     function securityItemAdd(securityItem) {
         let container = $('div[id="securityCategory_' + securityCategoryId + '"]').children('ul');
 
-        let content = '<div id="securityItem_' + securityItem.id + '" >' +
+        let content = $('<div id="securityItem_' + securityItem.id + '" >' +
                             '<li>' +
                                 '<span id="securityItem_text" contenteditable="true">' + securityItem.text + '</span>' +
                                 '<svg style="float: right" id="securityItem_delete_btn" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">' +
@@ -505,7 +507,7 @@ $(document).ready(function () {
                                     '<path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"></path>' +
                                 '</svg>' +
                             '</li>' +
-                        '</div>';
+                        '</div>');
 
         container.append(content);
     }
@@ -513,7 +515,7 @@ $(document).ready(function () {
     function ruleItemAdd(ruleItem) {
         let container = $('div[id="ruleCategory_' + ruleCategoryId + '"]').children('ul');
 
-        let content = '<div id="ruleItem_' + ruleItem.id + '">' +
+        let content = $('<div id="ruleItem_' + ruleItem.id + '">' +
                             '<li>' +
                                 '<span id="ruleItem_text" contenteditable="true">' + ruleItem.text + '</span>' +
                                 '<svg style="float: right" id="ruleItem_delete_btn" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">' +
@@ -521,7 +523,7 @@ $(document).ready(function () {
                                     '<path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"></path>' +
                                 '</svg>' +
                             '</li>' +
-                        '</div>'
+                        '</div>');
 
         container.append(content);
     }
@@ -529,16 +531,24 @@ $(document).ready(function () {
     function medicalCertificateAdd(medicalCertificate) {
         let container = $('ul[id="medicalCertificates"]');
 
-        let content = '<div id="medicalCertificate_' + medicalCertificate.id + '">' +
+        let content = $('<div id="medicalCertificate_' + medicalCertificate.id + '">' +
                         '<li>' +
                             '-' +
-                            '<span id="medicalCertificate_text" contenteditable="true">' + medicalCertificate.text + '</span>' +
-                            '<svg style="float: right" id="medicalCertificate_delete_btn" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">' +
-                                '<path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"></path>' +
-                                '<path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"></path>' +
-                            '</svg>' +
                         '</li>' +
-                    '</div>'
+                    '</div>');
+
+        let text = $('<span id="medicalCertificate_text" contenteditable="true">' + medicalCertificate.text + '</span>');
+        let deleteBtn = $('<svg style="float: right" id="medicalCertificate_delete_btn" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">' +
+                            '<path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"></path>' +
+                            '<path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"></path>' +
+                         '</svg>')
+
+        text.focusin(onTextFocusinListener)
+            .focusout(onTextFocusoutListener);
+
+        let liContainer = content.find('li');
+
+        liContainer.append(text).append(deleteBtn);
 
         container.append(content);
     }
