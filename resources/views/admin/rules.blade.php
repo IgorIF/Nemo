d40a4f222f6005975d7abce92a08190ba70cec4c
IgorIF <igarif1992@gmail.com>
2021-02-05 02:48:24 +0200

    update rules

--- resources/views/admin/rules.blade.php
+++ resources/views/admin/rules.blade.php
@@ -4,15 +4,23 @@
     <div id="ruleCategory_{{ $ruleCategories[0]->id }}" class="rule1">
         <ul>
             @foreach($ruleCategories[0]->ruleItems as $rule)
-                <div id="ruleItem_{{ $rule->id }}" >
-                    <li>
-                        <span id="number">{{ $loop->iteration }}</span>.
-                        <span id="ruleItem_text" contenteditable="true">{!! $rule->text !!}</span>
+                <div id="ruleItem_{{ $rule->id }}" style="display: table; width: 100%">
+                    <div style="display: table-cell">
+                        <li style="display: table; width: 100%">
+                            <div style="display: table-cell; width: 25px">
+                                <span id="number">{{ $loop->iteration }}</span>.
+                            </div>
+                            <div id="securityItem_text" contenteditable="true" style="display: table-cell">
+                                {!! $rule->text !!}
+                            </div>
+                        </li>
+                    </div>
+                    <div style="display: table-cell">
                         <svg style="float: right" id="ruleItem_delete_btn" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
                             <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"></path>
                             <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"></path>
                         </svg>
-                    </li>
+                    </div>
                 </div>
             @endforeach
         </ul>
@@ -27,15 +35,23 @@
     <div id="ruleCategory_{{ $ruleCategories[1]->id }}" class="rule2">
         <ul>
             @foreach($ruleCategories[1]->ruleItems as $rule)
-                <div id="ruleItem_{{ $rule->id }}" >
-                    <li>
-                        -
-                        <span id="ruleItem_text" contenteditable="true">{!! $rule->text !!}</span>
+                <div id="ruleItem_{{ $rule->id }}" style="display: table; width: 100%">
+                    <div style="display: table-cell">
+                        <li style="display: table; width: 100%">
+                            <div style="display: table-cell; width: 25px">
+                                -
+                            </div>
+                            <div id="securityItem_text" contenteditable="true" style="display: table-cell">
+                                {!! $rule->text !!}
+                            </div>
+                        </li>
+                    </div>
+                    <div style="display: table-cell">
                         <svg style="float: right" id="ruleItem_delete_btn" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
                             <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"></path>
                             <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"></path>
                         </svg>
-                    </li>
+                    </div>
                 </div>
             @endforeach
         </ul>
@@ -49,15 +65,23 @@
     <div id="ruleCategory_{{ $ruleCategories[2]->id }}" class="rule2">
         <ul>
             @foreach($ruleCategories[2]->ruleItems as $rule)
-                <div id="ruleItem_{{ $rule->id }}" >
-                    <li>
-                        <span id="number">{{ $loop->iteration }}</span>.
-                        <span id="ruleItem_text" contenteditable="true">{!! $rule->text !!}</span>
+                <div id="ruleItem_{{ $rule->id }}" style="display: table; width: 100%">
+                    <div style="display: table-cell">
+                        <li style="display: table; width: 100%">
+                            <div style="display: table-cell; width: 25px">
+                                <span id="number">{{ $loop->iteration }}</span>.
+                            </div>
+                            <div id="securityItem_text" contenteditable="true" style="display: table-cell">
+                                {!! $rule->text !!}
+                            </div>
+                        </li>
+                    </div>
+                    <div style="display: table-cell">
                         <svg style="float: right" id="ruleItem_delete_btn" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
                             <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"></path>
                             <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"></path>
                         </svg>
-                    </li>
+                    </div>
                 </div>
             @endforeach
         </ul>
