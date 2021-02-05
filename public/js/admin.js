d3e1ee86e1dfa6884bc3d272588ee82e04731015
IgorIF <igarif1992@gmail.com>
2021-02-05 02:48:45 +0200

    update rules

--- public/js/admin.js
+++ public/js/admin.js
@@ -618,36 +618,6 @@ $(document).ready(function () {
         deleteBtnContainer.append(deleteBtn);
 
         container.append(content);
-
-        /*let content = $('<div id="ruleItem_' + ruleItem.id + '">' +
-                            '<li>' +
-                            '</li>' +
-                        '</div>');
-
-        let text = $('<span id="ruleItem_text" contenteditable="true">' + ruleItem.text + '</span>');
-        let deleteBtn = $('<svg style="float: right" id="ruleItem_delete_btn" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">' +
-                            '<path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"></path>' +
-                            '<path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"></path>' +
-                        '</svg>');
-
-        text.focusin(onTextFocusinListener)
-            .focusout(onTextFocusoutListener);
-
-        let liContainer = content.find('li');
-
-        switch (Number(ruleCategoryId)) {
-            case 1:
-            case 3:
-                let prevNumber = $(container).find('li').last().find('span[id="number"]').text();
-                let number = $('<span id="number">' + (Number(prevNumber) + 1) + '</span>');
-                liContainer.append(number).append('. ');
-                break;
-            case 2:
-                liContainer.append('- ');
-        }
-
-        liContainer.append(text).append(deleteBtn);
-        container.append(content);*/
     }
 
     function medicalCertificateAdd(medicalCertificate) {
