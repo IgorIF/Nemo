1659f3100b4be4f46aa5eb88bfbb0f845b87b461
IgorIF <igarif1992@gmail.com>
2021-02-11 14:09:31 +0200

    add delete tooltip to new security item

--- public/js/admin.js
+++ public/js/admin.js
@@ -545,20 +545,24 @@ $(document).ready(function () {
                             '<div style="display: table-cell">' +
                                 '<li></li>' +
                             '</div>' +
-                            '<div style="display: table-cell"></div>' +
+                            '<div class="item-delete-container">' +
+                                '<svg id="securityItem_delete_btn" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash itemDeleteBtn" viewBox="0 0 16 16">' +
+                                    '<path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"></path>' +
+                                    '<path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"></path>' +
+                                '</svg>' +
+                                '<div id="deleteTooltip" class="delete-tooltip delete-security-item" tabindex="1">' +
+                                    'Удалить запись?' +
+                                    '<a class="button--border">Да</a>' +
+                                '</div>' +
+                            '</div>' +
                         '</div>');
 
         let text = $('<div id="securityItem_text" contenteditable="true">' +securityItem.text + '</div>');
-        let deleteBtn = $('<svg id="securityItem_delete_btn" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash itemDeleteBtn" viewBox="0 0 16 16">' +
-                            '<path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"></path>' +
-                            '<path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"></path>' +
-                        '</svg>');
 
         text.focusin(onTextFocusinListener)
             .focusout(onTextFocusoutListener);
 
         content.find('li').append(text);
-        content.find('div').last().append(deleteBtn);
 
         container.append(content);
     }
