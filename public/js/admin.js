5ae52a7b3d6f75c339b64bef599e50419753a5a5
IgorIF <igarif1992@gmail.com>
2021-02-11 21:54:19 +0200

    video add delete tooltip

--- public/js/admin.js
+++ public/js/admin.js
@@ -514,23 +514,24 @@ $(document).ready(function () {
     }
 
     function videoSlickAdd(video) {
-        let slide = $('<div class="slider video_' + video.id + '" style="text-align: center">' +
+        let slide = $('<div class="slider video_' + video.id + '">' +
                         '<div class="review_block">' +
                             '<div class="play_block">' +
-                                '<img src="storage/images/videos/' + video.image + '" />' +
+                                '<img src="storage/images/videos/' + video.image + '"  alt=""/>' +
                                 '<a id="play-video-review" class="video-play-button" href="' + video.url + '"><span></span></a>' +
                             '</div>' +
                         '</div>' +
+                        '<div class="video-delete-container">' +
+                            '<svg id="video_delete_btn" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash videoDeleteBtn" viewBox="0 0 16 16">' +
+                                '<path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"></path>' +
+                                '<path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"></path>' +
+                            '</svg>' +
+                            '<div id="deleteTooltip" class="delete-tooltip delete-video" tabindex="1">' +
+                                'Удалить видео?' +
+                                '<a class="button--border">Да</a>' +
+                            '</div>' +
+                        '</div>' +
                     '</div>');
-
-        let deleteBtn = $('<svg id="video_delete_btn" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash videoDeleteBtn" viewBox="0 0 16 16">' +
-                            '<path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"></path>' +
-                            '<path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"></path>' +
-                        '</svg>');
-
-        //deleteBtn.click(onVideoDeleteBtnClickListener);
-
-        slide.append(deleteBtn);
 
         slide.find('#play-video-review').click(onPlayVideoReviewIdClickListener);
         slide.find('.review_block .video-play-button').click(onVideoPlayButtonClassClickListener);
@@ -707,9 +708,11 @@ $(document).ready(function () {
     }
 
     function onDeleteBtnClickListener() {
+        console.log(11);
         let id = $(this).attr('id').split('_')[0];
         let tooltip = $(this).next('div[id="deleteTooltip"]');
 
+        console.log(tooltip);
         $(tooltip).fadeIn(300);
 
         $(tooltip).unbind('focusout').bind('focusout', function () {
