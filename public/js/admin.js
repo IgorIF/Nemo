8fd366f69da8ed828cb2937afb1a6506a400208d
IgorIF <igarif1992@gmail.com>
2021-02-11 23:49:25 +0200

    add medical certificate delete

--- public/js/admin.js
+++ public/js/admin.js
@@ -625,23 +625,26 @@ $(document).ready(function () {
                                     '</div>' +
                                 '</li>' +
                             '</div>' +
-                            '<div style="display: table-cell"></div>' +
-                        '</div>');
+                            '<div class="item-delete-container">' +
+                                '<svg id="medicalCertificate_delete_btn" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash itemDeleteBtn" viewBox="0 0 16 16">' +
+                                    '<path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"></path>' +
+                                    '<path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"></path>' +
+                                '</svg>' +
+                                '<div id="deleteTooltip" class="delete-tooltip delete-medical-certificate" tabindex="1">' +
+                                    'Удалить запись?' +
+                                    '<a class="button--border">Да</a>' +
+                                '</div>' +
+                             '</div>' +
+                         '</div>');
 
         let text = $('<div id="medicalCertificate_text" contenteditable="true" style="display: table-cell">' + medicalCertificate.text + '</div>');
-        let deleteBtn = $('<svg id="medicalCertificate_delete_btn" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash itemDeleteBtn" viewBox="0 0 16 16">' +
-                            '<path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"></path>' +
-                            '<path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"></path>' +
-                        '</svg>');
 
         text.focusin(onTextFocusinListener)
             .focusout(onTextFocusoutListener);
 
         let textContainer = content.find('li');
-        let deleteBtnContainer = content.find('div').last();
 
         textContainer.append(text);
-        deleteBtnContainer.append(deleteBtn);
 
         container.append(content);
     }
--- resources/views/admin/medical_certificates.blade.php
+++ resources/views/admin/medical_certificates.blade.php
@@ -14,12 +14,12 @@
                         </div>
                     </li>
                 </div>
-                <div style="display: table-cell; position: relative; width: 16px">
+                <div class="item-delete-container">
                     <svg id="medicalCertificate_delete_btn" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash itemDeleteBtn" viewBox="0 0 16 16">
                         <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"></path>
                         <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"></path>
                     </svg>
-                    <div class="delete-tooltip" tabindex="1">
+                    <div id="deleteTooltip" class="delete-tooltip delete-medical-certificate" tabindex="1">
                         Удалить запись?
                         <a class="button--border">Да</a>
                     </div>
