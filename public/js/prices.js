c6622f175e1441639c12198701bcf0befaefd8cb
IgorIF <igarif1992@gmail.com>
2021-03-01 00:51:48 +0200

    some code

--- public/js/prices.js
+++ public/js/prices.js
@@ -1,34 +1,39 @@
+let descriptions = {
+    "quantity": {
+        "trial": '<span class=\"medium\">Пробное занятие</span> предусмотрено для всех новых посетителей нашего акваклуба. Длительность занятия составляет: 30 минут. Проходит индивидуально с тренером.',
+        "oneoff": '<span class=\"medium\">Разовое занятие</span> предусмотрено для тех, кто уже посещал занятия. Отмена и перенос занятия до 18:00 предыдущего дня. Уведомление о смене тренера не предусмотрено',
+        "4": '<span class=\"medium\">Абонемент на 4 занятия</span> предусматривает 1 занятие в неделю.',
+        "8": '<span class=\"medium\">Абонемент на 8 занятий</span> предусматривает 2 занятия в неделю.',
+        "12": '<span class=\"medium\">Абонемент на 12 занятий</span> предусматривает 3 занятия в неделю.'
+    },
+    "swimming_pool": {
+        "small_pool": '<span class=\"medium\">Маленький бассейн </span> предназначен для индивидуальных занятий. Представляет собой круглый каркасный бассейн, диаметром - 3м, глубиной - 1,2м.',
+        "large_pool": '<span class=\"medium\">Большой бассейн</span> предназначен для групповых и индивидуальных занятий. Представляет собой прямоугольный бассейн, размерами: длина и ширина - 6х3м, глубина - 1,2м.'
+    },
+    "type_of_subscription": {
+        "standard": '<span class=\"medium\">Абонемент “Стандарт”</span> предусматривает отмену занятия до 18.00 предыдущего дня. Срок действия абонемента: 4 недели + 2 недели заморозки. Уведомление о смене тренера не предусмотрено.',
+        "loyal": '<span class=\"medium\">Абонемент “Лояльный”</span> позволяет получать уведомление о смене тренера и отменять занятие день в день без потери занятия. Срок действия абонемента: 4 недели + 2 недели заморозки.'
+    }
+};
+
 $(document).ready(function () {
 
     $('form[class="calculator"]').each(function (i, e) {
         let prices = $(e).data('prices');
-        let checkedFieldsValues = initCheckedFields();
         let oldPrice = getOldPrice(prices);
 
-        singleBlockStretch(e);
-        initCalculator(e, checkedFieldsValues, prices, oldPrice);
+        initDescriptions(e);
+        initCalculator(e, prices, oldPrice);
 
         $(e).find('input').click(function () {
-            checkedFieldsValues = initCheckedFields();
-            initCalculator(e, checkedFieldsValues, prices);
-            singleBlockStretch(e);
+            initDescriptions(e);
+            initCalculator(e, prices, oldPrice);
         });
-
-        function initCheckedFields() {
-
-            let checkedFieldsValues = [];
-
-            $(e).find('input:checked').each(function (i, e) {
-                checkedFieldsValues.push($(e).val());
-            })
-
-            return checkedFieldsValues;
-        }
-    })
+    });
 
 });
 
-function initCalculator (filial, chFields, prices, oldPrice) {
+function initCalculator (filial, prices, oldPrice) {
     let quantity = $(filial).find('div[id="quantity"]');
     let swimmingPool = $(filial).find('div[id="swimming_pool"]');
     let typeOfSubscription = $(filial).find('div[id="type_of_subscription"]');
@@ -42,34 +47,52 @@ function initCalculator (filial, chFields, prices, oldPrice) {
     if (Number.isInteger(price)) {
         $(swimmingPool).hide();
         $(typeOfSubscription).hide();
-        showAndHidePrices(filial, price, oldPrice);
+        showPrices(filial, price, oldPrice);
+        fillLinks(filial, price);
     } else {
+        $(swimmingPool).css('max-width', '303px').css('width', '49%');
+        $(typeOfSubscription).css('max-width', '303px').css('width', '49%');
         $(swimmingPool).show();
         $(typeOfSubscription).show();
 
         price = prices[quantityVal][swimmingPoolVal];
 
         if (Number.isInteger(price)) {
+            $(swimmingPool).css('max-width', 'unset').css('width', '100%');
             $(typeOfSubscription).hide();
-            showAndHidePrices(filial, price, oldPrice);
+            showPrices(filial, price, oldPrice);
+            fillLinks(filial, price);
         } else {
             price = prices[quantityVal][typeOfSubscriptionVal];
 
             if (Number.isInteger(price)) {
+                $(typeOfSubscription).css('max-width', 'unset').css('width', '100%');
                 $(swimmingPool).hide();
-                showAndHidePrices(filial, price, oldPrice);
+                showPrices(filial, price, oldPrice);
+                fillLinks(filial, price);
             } else {
                 price = prices[quantityVal][swimmingPoolVal][typeOfSubscriptionVal];
 
                 if (Number.isInteger(price)) {
-                    showAndHidePrices(filial, price, oldPrice);
+                    showPrices(filial, price, oldPrice);
+                    fillLinks(filial, price);
                 }
             }
         }
     }
 }
 
-function showAndHidePrices(filial, newPrice, oldPrice) {
+function initDescriptions (filial) {
+    let inputsChecked = $(filial).find('input:checked');
+
+    $(inputsChecked).each(function (i, e) {
+        let descriptionContainer = $(e).parents('div[class="section"]').find('p[class="note"]');
+        $(descriptionContainer).html(descriptions[$(e).attr('name')][$(e).attr('value')]);
+    });
+
+}
+
+function showPrices(filial, newPrice, oldPrice) {
     let priceContainer = $(filial).find('div[class="price-row"]');
     let oldPriceContainer = $(priceContainer).find('div[class="old-price"]');
     oldPriceContainer.find('span').text(oldPrice);
@@ -87,38 +110,96 @@ function getOldPrice(prices) {
         return prices['oneoff']['small_pool'];
 }
 
-function singleBlockStretch(filial) {
-    let blockSections = $(filial).find('div[class="list-hidden"]').find('div[class="section"]:visible');
-
-    if (blockSections.length % 2 === 1) {
-        $(blockSections.last()).css('max-width', 'unset').css('width', '100%');
-    } else {
-        $(blockSections).each(function (i, e) {
-            $(e).css('max-width', '303px').css('width', '49%');
-        })
-    }
-}
-
 function fillLinks(filial, price) {
     let signUpBtn = $(filial).find('a[class="bt-1"]');
     let payOnlineBtn = $(filial).find('a[class="bt-2"]');
 
-    let filialCode;
+    let signUpBtnLink;
+    let payOnlineBtnLink;
 
-    if ($(filial).attr('id') !== 'nekrasovka') {
+    let quantityVal = $(filial).find('input[name="quantity"]:checked').val();
+
+    /////////////////////// signUpBtn
+    let yclientsCode;
+
+    if ($(filial).attr('id') === 'nekrasovka') {
+        // TODO ссылка некрасова
+    } else {
         switch ($(filial).attr('id')) {
             case 'prazhskaya':
-                filialCode = '60559';
+                yclientsCode = '60559';
                 break;
             case 'akademicheskaya':
-                filialCode = '124032';
+                yclientsCode = '124032';
                 break;
             case 'maryino':
-                filialCode = '228760';
+                yclientsCode = '228760';
         }
 
-        let signUpBtnLink = 'https://n' + filialCode + '.yclients.com/';
+        signUpBtnLink = 'https://n' + yclientsCode + '.yclients.com/';
+
         $(signUpBtn).attr('href', signUpBtnLink);
+
+        if (quantityVal !== 'trial' && quantityVal !== 'oneoff') {
+            $(signUpBtn).unbind('click').bind('click', showTrialPopup);
+        } else {
+            $(signUpBtn).unbind('click');
+        }
+
     }
 
+    /////////////////////// payOnlineBtn
+    let serviceName = '';
+    let add;
+
+    add = $(filial).find('input[name="quantity"]:checked:visible');
+    if (add.length > 0)
+        serviceName = serviceName + 'Количестово занятий: ' + $(add).next("span:first").text().toLowerCase();
+
+    add = $(filial).find('input[name="swimming_pool"]:checked:visible');
+    if (add.length > 0)
+        serviceName = serviceName + ', Бассейн: ' + $(add).next("span:first").text().toLowerCase();
+
+    add = $(filial).find('input[name="type_of_subscription"]:checked:visible');
+    if (add.length > 0)
+        serviceName = serviceName + ', Вид абонемента: ' + $(add).next("span:first").text().toLowerCase();
+
+    let key = false;
+
+    switch ($(filial).attr('id')) {
+        case 'prazhskaya':
+            key = 'G%2FncjmNEBe84x4lEfgf3dyIqwGWQVDf7HKlnEBr0Dgc%3D';
+            break;
+        case 'akademicheskaya':
+            key = 'Wy4g8yADEp%2FmSFHDKbbHqGOrWnEzE8kDIrSrapVc0Z8%3D';
+            break;
+        case 'maryino':
+            key = 'TOJugDeSdrrhNrSj8vsHLughHsNGQFliUmq8X4yCPkE%3D';
+            break;
+        case 'nekrasovka':
+            key = 'LX21USWpnEKqvvVyPmda5tammqzcCmeoOV6NnCYfQ8k=';
+            break;
+    }
+
+    if (quantityVal === 'trial' || quantityVal === 'oneoff') {
+        payOnlineBtnLink = signUpBtnLink;
+    } else {
+        payOnlineBtnLink = 'https://partner.rficb.ru/alba/input/?name=' + serviceName +'&cost=' + price + '&key=' + key + '&default_email=&order_id=0';
+    }
+
+    $(payOnlineBtn).attr('href', payOnlineBtnLink);
+}
+
+function showTrialPopup(e) {
+    e.preventDefault();
+
+    $('.hidden-form').attr('value', $(this).attr('data-type'));
+
+    $('.popup').slideDown();
+    $('.popup-overlay').css('display', 'block');
+
+    $('#js-close-popup, .popup-overlay').on('click', function(e) {
+        $('.popup').slideUp();
+        $('.popup-overlay').css('display', 'none');
+    });
 }
