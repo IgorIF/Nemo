4cb29fed04a6c6c61326817a7b28318b3c0b72a1
IgorIF <igarif1992@gmail.com>
2021-08-13 16:31:03 +0300

    some changes

--- resources/views/site/home/offer.blade.php
+++ resources/views/site/home/offer.blade.php
@@ -1,5 +1,8 @@
 <section class="offer">
-    <div class="offer__img">{{--<img src="{{ asset('images/dist/offer-img.jpg') }}" alt="">--}}</div>
+    <div class="offer__img">
+        <img class="offer__img-desk" src="{{ asset('images/dist/offer-img.jpg') }}" alt="">
+        <img class="offer__img-mob" src="{{ asset('images/dist/offer-img-mob.jpg') }}" alt="">
+    </div>
     <div class="container">
         <h1 class="offer__title">{{ $texts->get(3)->text }}</h1>
         <p>{!! $texts->get(4)->text !!}</p>
